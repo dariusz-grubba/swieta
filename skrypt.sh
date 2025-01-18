0787818c7e0f454f905112b74888dfc5a3fd9f6d
Dariusz Grubba <dariusz.grubba.dg@gmail.com>
2025-01-18 06:53:20 -0500

    dupaaaa

--- skrypt.sh
+++ skrypt.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+mkdir "Kartki swiateczne" "Kartki swiateczne/Kotki" "Kartki swiateczne/Pieski"
+mkdir "Pozostale" "Pozostale/Listy zakupow" "Pozostale/Przepisy" "Pozostale/Listy zakupow/Swieta 2022" "Pozostale/Listy zakupow/Swieta 2023"
+mkdir "Prace domowe" "Zadania"
 
-mkdir -p "Kartki swiateczne/Kotki" "Kartki swiateczne/Pieski"
-mkdir -p "Pozostale/Listy zakupow/Swieta 2022" "Pozostale/Listy zakupow/Swieta 2023"
-mkdir -p "Pozostale/Przepisy" "Prace domowe" "Zadania"
-
-if [[ $1 =~ [0-9]{4} ]]
+if [[ $1 =~ [0-9]{4} ]] ||  [[ $2 =~ [0-9]+ ]] ||  [[ $3 =~ [0-9]+ ]] 
 then
+        touch "Pozostale/Przepisy/Lista zakupow 2022.txt"
+        touch "Pozostale/Przepisy/Lista zakupow 2023.txt"
+        echo "świeża mięta
+1 kg jabłek
+1 kg cytryn
+500 g kiszonej kapusty
+1 mała główka świeżej kapusty
+80 g suszonych, leśnych grzybów
+200 g suszonych jabłek, śliwek, gruszek
+4 kg ziemniaków
+500 g drobnej, białej fasoli
+bułka tarta
+1 kg cukru
+1 l barszczu w kartonie" > "Pozostale/Przepisy/Lista zakupow ${1}.txt"
 
+        mv "Pozostale/Przepisy/Lista zakupow 2022.txt" "Pozostale/Listy zakupow/Swieta 2022/"
+        mv "Pozostale/Przepisy/Lista zakupow 2023.txt" "Pozostale/Listy zakupow/Swieta 2023/"
 
-echo "1 kg jablek
-4 kg gruszek
-500 giszonej kapusty
-80 g ziol" > "Pozostale/Przepisy/Lista zakupow${1}.txt"
+        egrep  "[1-9][0-9]* kg" "Pozostale/Listy zakupow/Swieta ${1}/Lista zakupow ${1}.txt" > "Pozostale/Listy zakupow/Ciezkie.txt"
 
-else 
-	echo "argument chujowy"
+	
+	head -$2 "Pozostale/Listy zakupow/Swieta ${1}/Lista zakupow ${1}.txt" > "Pozostale/Listy zakupow/Wybrane zakupy.txt"
+	tail -$3 "Pozostale/Listy zakupow/Swieta ${1}/Lista zakupow ${1}.txt" >> "Pozostale/Listy zakupow/Wybrane zakupy.txt"
+	
+touch archiwum.tar.gz
+tar -cvzf  "archiwum.tar.gz" .
+   
+else
+        echo "arg 1 do dupy"
 fi
-
-
-
-
